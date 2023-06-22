6b9fa5fd002c3e454f35c1e74e6261c7f7c4affc
arjun <arjunrokaya@gmail.com>
2023-06-22 09:01:46 +0545

    project files

---
+++ src/main/java/com/bway/springproject/Springproject8amApplication.java
@@ -0,0 +1,13 @@
+package com.bway.springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springproject8amApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springproject8amApplication.class, args);
+	}
+
+}
